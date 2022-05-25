3b4bf9a9d4e9f775c88b542641186898f5b43227
francois <francois.nadeau.1@umontreal.ca>
2022-05-25 11:04:49 -0400

    "Derivatives WIP - Updated setup.py"

--- setup.py
+++ setup.py
@@ -3,18 +3,22 @@ from setuptools import setup
 SETUP_REQUIRES = [
     'pandas ~= 1.4.2', 'nibabel ~= 3.2.2', 'numpy ~= 1.22.4',
 ]
+AUTHOR = 'Francois Nadeau'
+EMAIL = 'francois.nadeau1@gmail.com'
 
 setup(
     name='bidspathlib',
-    version='0.0.6.3',
+    version='0.0.6.4',
     packages=[
         'core', 'core.tests', 'core.bids_dir', 'core.bids_file',
         'constants', 'functions'
     ],
     url='https://github.com/FrancoisNadeau/bids_path',
     license='MIT',
-    author='francois',
-    author_email='francois.nadeau1@gmail.com',
+    author=AUTHOR,
+    maintainer=AUTHOR,
+    author_email=EMAIL,
+    maintainer_email=EMAIL,
     description='Python package applying Path object machinery to BIDS datasets.',
     classifiers=[
         'Intended Audience :: Science/Research',
