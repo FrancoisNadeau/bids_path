0ed432e2e6d553824f38d573c0ec8b90e2b61c68
FrancoisNadeau <francois.nadeau1@gmail.com>
2022-06-08 13:14:24 -0400

    "Added setuptools and wheel packages to requirements.txt"

---
+++ build/lib/core/bids_file/LicenseFile.py
@@ -0,0 +1,33 @@
+"""
+Class representing a LICENSE file in a BIDS dataset.
+
+"""
+
+import os
+from pprint import pprint
+from typing import Text, Union
+
+from ..BIDSFileAbstract import BIDSFileAbstract
+
+__path__ = [os.path.join('../..', '__init__.py')]
+
+
+class ReadMeFile(BIDSFileAbstract):
+    """
+    Class representing a README file in a BIDS dataset.
+
+    """
+    __slots__ = ()
+    def __type__(self): return type(self)
+
+    def __instancecheck__(self, instance) -> bool:
+        conditions = (hasattr(instance, 'entities'),
+                      os.path.basename(instance) == 'README')
+        return all(conditions)
+
+    def __init__(self, src: Union[Text, os.PathLike], **kwargs):
+        super().__init__(src, **kwargs)
+
+    def view(self):
+        _contents = self.path.read_text().strip()
+        pprint(_contents)
