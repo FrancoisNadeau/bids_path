22b5c04ea86159fc87416d6861dab22c91b5b6fd
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2022-06-10 13:00:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ EMAIL = 'francois.nadeau1@gmail.com'
 
 setup(
     name='bidspathlib',
-    version='0.0.9.6.6',
+    version='0.0.9.6.7',
     packages=[
         'core', 'core.tests', 'core.bids_dir', 'core.bids_file',
         'constants', 'functions'
