ab465e6b4d9b9305ca4c513bbe236dc96aa437f4
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2022-06-10 18:39:24 -0400

    Update BIDSPathConstants.py

--- constants/BIDSPathConstants.py
+++ constants/BIDSPathConstants.py
@@ -52,6 +52,62 @@ BVE_MESSAGE: Text = "\n".join((
 ))
 
 SUFFIX_PATTERNS: Pattern = re.compile('|'.join(SUFFIX_STRINGS))
+BIDS_RECOMMENDED: Dict = \
+    {
+  "Manufacturer": {
+    "description": "Manufacturer of the equipment that produced the measurements.",
+    "dicom_tag": "Corresponds to DICOM Tag 0008, 0070 Manufacturer."
+  },
+  "ManufacturersModelName": {
+    "description": "Manufacturer's model name of the equipment that produced the measurements.",
+    "dicom_tag": "Corresponds to DICOM Tag 0008, 1090 Manufacturers Model Name."
+  },
+  "DeviceSerialNumber": {
+    "description": "The serial number of the equipment that produced the measurements.\nA pseudonym can also be used to prevent the equipment from being identifiable, so long as each pseudonym is unique within the dataset.",
+    "dicom_tag": "Corresponds to DICOM Tag 0018, 1000 DeviceSerialNumber."
+  },
+  "StationName": {
+    "description": "Institution defined name of the machine that produced the measurements.",
+    "dicom_tag": "Corresponds to DICOM Tag 0008, 1010 Station Name."
+  },
+  "SoftwareVersions": {
+    "description": "Manufacturer's designation of software version of the equipment that produced the measurements.",
+    "dicom_tag": "Corresponds to DICOM Tag 0018, 1020 Software Versions."
+  },
+  "MagneticFieldStrength": {
+    "description": "Nominal field strength of MR magnet in Tesla.",
+    "dicom_tag": "Corresponds to DICOM Tag 0018, 0087 Magnetic Field Strength."
+  },
+  "ReceiveCoilName": {
+    "description": "Information describing the receiver coil.\nNot all vendors populate that DICOM Tag, in which case this field can be derived from an appropriate private DICOM field.",
+    "dicom_tag": "Corresponds to DICOM Tag 0018, 1250 Receive Coil Name"
+  },
+  "ReceiveCoilActiveElements": {
+    "description": "Information describing the active or selected elements of the receiver coil.\nThe vendor-defined terminology for active coil elements can go in this field.",
+    "dicom_tag": "None"
+  },
+  "GradientSetType": {
+    "description": "It should be possible to infer the gradient coil from the scanner model.\nIf not, for example because of a custom upgrade or use of a gradient insert set, then the specifications of the actual gradient coil should be reported independently.",
+    "dicom_tag": "None"
+  },
+  "MRTransmitCoilSequence": {
+    "description": "This is a relevant field if a non-standard transmit coil is used.",
+    "dicom_tag": "Corresponds to DICOM Tag 0018, 9049 MR Transmit Coil Sequence."
+  },
+  "MatrixCoilMode": {
+    "description": "(If used) A method for reducing the number of independent channels by combining in analog the signals from multiple coil elements.\nThere are typically different default modes when using un-accelerated or accelerated (for example, \"GRAPPA\", \"SENSE\") imaging.",
+    "dicom_tag": "None"
+  },
+  "CoilCombinationMethod": {
+    "description": "Most fMRI studies using phased-array coils use root-sum-of-squares (rSOS) combination, but other methods exist.\nThe image reconstruction is changed by the coil combination method (as for the matrix coil mode above), so anything non-standard should be reported.",
+    "dicom_tag": "None"
+  },
+  "HardcopyDeviceSoftwareVersion": {
+    "description": "Manufacturer's designation of the software of the device that created this Hardcopy Image (the printer).",
+    "dicom_tag": "Corresponds to DICOM Tag 0018, 101A Hardcopy Device Software Version."
+  }
+}
+
 
 Entities: Type[Tuple] = namedtuple('Entities', field_names=ENTITIES_ORDER)
 EntityStrings: Type[Tuple] = namedtuple('Entities', field_names=ENTITY_STRINGS)
